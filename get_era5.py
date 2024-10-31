0a9e614e9eb4a17be58e6b45d3408999b6bd29d0
Mohamed-01-git <mohamedelbadri68@gmail.com>
2024-10-31 09:27:24 +0100

    this code to get era5 data

--- get_era5.py
+++ get_era5.py
@@ -18,37 +18,24 @@ import numpy as np
 CDSAPI_URL = 'https://cds-beta.climate.copernicus.eu/api'
 CDSAPI_KEY = '328ae3b8-36e2-4c27-9892-c7a55f386cdf'
 c = cdsapi.Client(url=CDSAPI_URL, key=CDSAPI_KEY)
+DATADIR ="/home/mohamed/EHTPIII/MODELISATION/DATA"   
     
-DATADIR = "/home/mohamed/EHTPIII/MODELISATION/DATA"  # Update with your actual data directory
-config = {
-    'list_vars': ['2m_temperature', 'total_precipitation'],  # Example variables, adjust as needed
-    'obsstarty': 1993,  # Starting year
-    'obsendy': 2016,  # Ending year
-    'start_month': 1  # Start month (January)
-}
+obs_fname = '/home/mohamed/EHTPIII/MODELISATION/DATA/era5_monthly_stmonth{"NOVEMBER"}_{1993}-{2016}.grib'
 
-# Create the output filename
-obs_fname = '{fpath}/era5_monthly_stmonth{start_month:02d}_{hcstarty}-{hcendy}.grib'.format(
-    fpath=DATADIR,
-    start_month=config['start_month'],
-    hcstarty=config['obsstarty'],
-    hcendy=config['obsendy']
-)
-
-print(obs_fname)
-
-# Retrieve data from the CDS
 c.retrieve(
     'reanalysis-era5-single-levels-monthly-means',
     {
         'product_type': 'monthly_averaged_reanalysis',
-        'variable': config['list_vars'],
-        'year': ['{}'.format(yy) for yy in range(config['obsstarty'], config['obsendy'] + 2)],  # Including one extra year
-        'month': ['{:02d}'.format((config['start_month'] + leadm - 1) % 12 + 1) for leadm in range(6)],  # Adjust month calculation
+        'variable': [
+            "2m_temperature",
+            "total_precipitation",
+        ],
+        'year': [str(year) for year in range(1993, 2017)],  # List of years as strings
+        'month': ['{:02d}'.format(month) for month in range(1, 13)],  # All months from January to December as zero-padded strings
         'time': '00:00',
-        'grid': '1/1',  # Interpolation grid
-        'area': '45/-30/-2.5/60',  # Area defined by N,W,S,E
-        'data_format': 'grib',
+        'grid': '1/1',  # Interpolating ERA5 data to 1x1 degree grid
+        'area': [45, -30, -2.5, 60],  # North, West, South, East coordinates
+        'format': 'grib',
     },
     obs_fname
 )
